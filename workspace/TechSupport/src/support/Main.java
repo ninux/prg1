376230ce6646a4f8aec5328abe5a9ee888826662
Nino Ninux <nino.ninux@gmail.com>
2013-11-01 21:53:46 +0100

    began to work with random numbers

---
+++ workspace/TechSupport/src/support/Main.java
@@ -0,0 +1,12 @@
+package support;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		// create a support system and start it
+		SupportSystem sys = new SupportSystem();
+		sys.start();
+		
+	}	
+}
