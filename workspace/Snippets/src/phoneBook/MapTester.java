7986f44824646da656072ac38e808614e7b2c8ea
Nino Ninux <nino.ninux@gmail.com>
2013-11-09 11:42:41 +0100

    began to solve OOP7 selfstudy-questions

---
+++ workspace/Snippets/src/phoneBook/MapTester.java
@@ -0,0 +1,23 @@
+package phoneBook;
+
+import java.util.HashMap;
+
+public class MapTester
+{
+	HashMap<String, String> myMap = new HashMap<String, String>();
+	
+	public MapTester()
+	{
+		
+	}
+	
+	public void enterNumber(String name, String number)
+	{
+		myMap.put(name, number);
+	}
+	
+	public String lookupNumber(String name)
+	{
+		return myMap.get(name);
+	}
+}
